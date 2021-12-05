cf4fc3079d0a2f20259e1631e191dedbdce19835
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2021-12-05 16:37:37 +0100

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 powershell.exe Stop-Service -Name "wuauserv" -Force
-powershell.exe -noprofile -executionpolicy bypass Install-PackageProvider -Name NuGet -Force
-powershell.exe -noprofile -executionpolicy bypass Install-Module PSWindowsUpdate -Force
-powershell.exe -noprofile -executionpolicy bypass Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
+powershell.exe -executionpolicy bypass Install-PackageProvider -Name NuGet -Force
+powershell.exe -executionpolicy bypass Install-Module PSWindowsUpdate -Force
+powershell.exe -executionpolicy bypass Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
