d62d2d198b35812951fd78b9cdfc1eb83187c02e
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-06-16 21:04:47 +0200

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,12 +1,3 @@
-# Self-elevate the script if required
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
- if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-  Exit
- }
-}
-
 Stop-Service -Name "wuauserv" -Force
 Install-PackageProvider -Name NuGet -Force
 Install-Module PSWindowsUpdate -Force
